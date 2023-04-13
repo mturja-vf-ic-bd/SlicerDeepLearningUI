34bfb83196fc30e4c7c578cee536b81369c2ca28
Md Asadullah Turja <mturja.vf.ic.bd@gmail.com>
2023-04-13 09:13:20 -0400

    Added geometry image module

---
+++ GeometryImage/GeometryImage.py
@@ -0,0 +1,372 @@
+import logging
+try:
+    import scipy
+except ImportError:
+    slicer.util.pip_install('scipy==1.7.1')
+
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+
+from Asynchrony import Asynchrony
+from geometry_image.tools.run import run_geom_image
+
+
+#
+# GeometryImage
+#
+
+class GeometryImage(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "GeometryImage"  # TODO: make this more human readable by adding spaces
+        self.parent.categories = [
+            "Deep Learning"]  # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+        self.parent.contributors = [
+            "Md Asadullah Turja (UNC Chapel Hill)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        # TODO: update with short description of the module and a link to online module documentation
+        self.parent.helpText = """
+Geometry image was proposed by Gu et al. (https://hhoppe.com/gim.pdf) where geometry is resampled into a completely regular 2D grid. The process involves cutting the surface into a
+disk using a network of cut paths, and then mapping the boundary of this disk to a square. Both geometry and other signals are stored as 2D grids, with grid samples in
+implicit correspondence.
+"""
+        # TODO: replace with organization, grant and thanks
+        self.parent.acknowledgementText = """Dummy"""
+
+
+#
+# GeometryImageWidget
+#
+
+class GeometryImageWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+    def __init__(self, parent=None):
+        """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        VTKObservationMixin.__init__(self)  # needed for parameter node observation
+        self._asynchrony = None
+        self.outputDirectory = None
+        self.inputDirectory = None
+        self.templateDirectory = None
+        self.logic = None
+        self._parameterNode = None
+        self._updatingGUIFromParameterNode = False
+
+    def setup(self):
+        """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # Load widget from .ui file (created by Qt Designer).
+        # Additional widgets can be instantiated manually and added to self.layout.
+        uiWidget = slicer.util.loadUI(self.resourcePath('UI/GeometryImage.ui'))
+        self.uiWidget = uiWidget
+        self.layout.addWidget(uiWidget)
+        self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+        # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+        # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+        # "setMRMLScene(vtkMRMLScene*)" slot.
+        uiWidget.setMRMLScene(slicer.mrmlScene)
+
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = GeometryImageLogic()
+
+        # Connections
+
+        # These connections ensure that we update parameter node when scene is closed
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+        self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+        # These connections ensure that whenever user changes some settings on the GUI, that is saved in the MRML scene
+        # (in the selected parameter node).
+        # Buttons
+        self.ui.InputPushButton.connect('clicked(bool)', self.populateInputLineEdit)
+        self.ui.TemplatePushButton.connect('clicked(bool)', self.populateTemplateLineEdit)
+        self.ui.OutputPushButton.connect('clicked(bool)', self.populateOutputLineEdit)
+        self.ui.GeneratePushButton.connect('clicked(bool)', self.onApplyButton)
+
+        # Make sure parameter node is initialized (needed for module reload)
+        self.initializeParameterNode()
+
+    def populateInputDirectory(self, mode="input"):
+        """
+    Populates self.inputPath
+    """
+        dialog = qt.QFileDialog(self.uiWidget)
+        if mode == "input":
+            dialog.setFileMode(qt.QFileDialog.Directory)
+        else:
+            dialog.setFileMode(qt.QFileDialog.AnyFile)
+        dialog.setViewMode(qt.QFileDialog.Detail)
+        if dialog.exec_():
+            directoryPath = dialog.selectedFiles()
+            logging.debug("onDirectoryChanged: {}".format(directoryPath))
+            if mode == "input":
+                self.inputDirectory = directoryPath[0]
+                self.ui.InputLineEdit.text = directoryPath[0]
+            elif mode == "output":
+                self.outputDirectory = directoryPath[0]
+                self.ui.OutputLineEdit.text = directoryPath[0]
+            else:
+                self.templateDirectory = directoryPath[0]
+                self.ui.TemplateLineEdit.text = directoryPath[0]
+
+    def populateInputLineEdit(self):
+        self.populateInputDirectory("input")
+
+    def populateTemplateLineEdit(self):
+        self.populateInputDirectory("template")
+
+    def populateOutputLineEdit(self):
+        self.populateInputDirectory("output")
+
+    def cleanup(self):
+        """
+    Called when the application closes and the module widget is destroyed.
+    """
+        self.removeObservers()
+
+    def enter(self):
+        """
+    Called each time the user opens this module.
+    """
+        # Make sure parameter node exists and observed
+        self.initializeParameterNode()
+
+    def exit(self):
+        """
+    Called each time the user opens a different module.
+    """
+        # Do not react to parameter node changes (GUI wlil be updated when the user enters into the module)
+        self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+
+    def onSceneStartClose(self, caller, event):
+        """
+    Called just before the scene is closed.
+    """
+        # Parameter node will be reset, do not use it anymore
+        self.setParameterNode(None)
+
+    def onSceneEndClose(self, caller, event):
+        """
+    Called just after the scene is closed.
+    """
+        # If this module is shown while the scene is closed then recreate a new parameter node immediately
+        if self.parent.isEntered:
+            self.initializeParameterNode()
+
+    def initializeParameterNode(self):
+        """
+    Ensure parameter node exists and observed.
+    """
+        # Parameter node stores all user choices in parameter values, node selections, etc.
+        # so that when the scene is saved and reloaded, these settings are restored.
+
+        self.setParameterNode(self.logic.getParameterNode())
+
+    def setParameterNode(self, inputParameterNode):
+        """
+    Set and observe parameter node.
+    Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+    """
+
+        if inputParameterNode:
+            self.logic.setDefaultParameters(inputParameterNode)
+
+        # Unobserve previously selected parameter node and add an observer to the newly selected.
+        # Changes of parameter node are observed so that whenever parameters are changed by a script or any other module
+        # those are reflected immediately in the GUI.
+        if self._parameterNode is not None:
+            self.removeObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+        self._parameterNode = inputParameterNode
+        if self._parameterNode is not None:
+            self.addObserver(self._parameterNode, vtk.vtkCommand.ModifiedEvent, self.updateGUIFromParameterNode)
+
+        # Initial GUI update
+        self.updateGUIFromParameterNode()
+
+    def updateGUIFromParameterNode(self, caller=None, event=None):
+        """
+    This method is called whenever parameter node is changed.
+    The module GUI is updated to show the current state of the parameter node.
+    """
+
+        if self._parameterNode is None or self._updatingGUIFromParameterNode:
+            return
+
+        # Make sure GUI changes do not call updateParameterNodeFromGUI (it could cause infinite loop)
+        self._updatingGUIFromParameterNode = True
+        self.ui.GeneratePushButton.enabled = True
+        # Update buttons states and tooltips
+        # All the GUI updates are done
+        self._updatingGUIFromParameterNode = False
+
+    def updateParameterNodeFromGUI(self, caller=None, event=None):
+        """
+    This method is called when the user makes any change in the GUI.
+    The changes are saved into the parameter node (so that they are restored when the scene is saved and loaded).
+    """
+
+        if self._parameterNode is None or self._updatingGUIFromParameterNode:
+            return
+
+    def onApplyButton(self):
+        """
+        Run processing when user clicks "Apply" button.
+        """
+
+        try:
+            self._asynchrony = Asynchrony(
+                lambda: self.logic.process(
+                    self.ui.InputLineEdit.text,
+                    self.ui.TemplateLineEdit.text,
+                    self.ui.OutputLineEdit.text,
+                    512,
+                    self.ui.progressBar)
+            )
+            self._asynchrony.Start()
+        except Exception as e:
+            slicer.util.errorDisplay("Failed to compute results: " + str(e))
+            import traceback
+            traceback.print_exc()
+
+
+#
+# GeometryImageLogic
+#
+
+class GeometryImageLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+    def __init__(self):
+        """
+    Called when the logic class is instantiated. Can be used for initializing member variables.
+    """
+        ScriptedLoadableModuleLogic.__init__(self)
+
+    def setDefaultParameters(self, parameterNode):
+        """
+    Initialize parameter node with default settings.
+    """
+        pass
+
+    def process(self, InputDirectory, TemplateDirectory, OutputDirectory, r=512, progressBar=None):
+        """
+        Run the processing algorithm.
+        Can be used without GUI widget.
+        :param inputVolume: volume to be thresholded
+        :param outputVolume: thresholding result
+        :param imageThreshold: values above/below this threshold will be set to 0
+        :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
+        :param showResult: show output volume in slice viewers
+        """
+
+        if not InputDirectory or not TemplateDirectory or not OutputDirectory:
+            raise ValueError("Input or Template or Output directory is invalid")
+
+        import time
+        startTime = time.time()
+        logging.info('Processing started')
+
+        # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
+        cliParams = {
+            'sub_dir': InputDirectory,
+            'sphere_template': TemplateDirectory,
+            'resolution': r,
+            'output': OutputDirectory,
+            'progressBar': progressBar
+        }
+        run_geom_image(cliParams)
+        stopTime = time.time()
+        Asynchrony.RunOnMainThread(
+            lambda: slicer.util.infoDisplay('Processing completed in {0:.2f} seconds'.format(stopTime - startTime)))
+        logging.info('Processing completed in {0:.2f} seconds'.format(stopTime - startTime))
+
+
+#
+# GeometryImageTest
+#
+
+class GeometryImageTest(ScriptedLoadableModuleTest):
+    """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+    def setUp(self):
+        """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+        slicer.mrmlScene.Clear()
+
+    def runTest(self):
+        """Run as few or as many tests as needed here.
+    """
+        self.setUp()
+        self.test_GeometryImage1()
+
+    def test_GeometryImage1(self):
+        """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+        self.delayDisplay("Starting the test")
+
+        # Get/create input data
+
+        # import SampleData
+        # registerSampleData()
+        # inputVolume = SampleData.downloadSample('GeometryImage1')
+        # self.delayDisplay('Loaded test data set')
+        #
+        # inputScalarRange = inputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(inputScalarRange[0], 0)
+        # self.assertEqual(inputScalarRange[1], 695)
+        #
+        # outputVolume = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode")
+        # threshold = 100
+        #
+        # # Test the module logic
+        #
+        # logic = GeometryImageLogic()
+        #
+        # # Test algorithm with non-inverted threshold
+        # logic.process(inputVolume, outputVolume, threshold, True)
+        # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        # self.assertEqual(outputScalarRange[1], threshold)
+        #
+        # # Test algorithm with inverted threshold
+        # logic.process(inputVolume, outputVolume, threshold, False)
+        # outputScalarRange = outputVolume.GetImageData().GetScalarRange()
+        # self.assertEqual(outputScalarRange[0], inputScalarRange[0])
+        # self.assertEqual(outputScalarRange[1], inputScalarRange[1])
+
+        self.delayDisplay('Test passed')
